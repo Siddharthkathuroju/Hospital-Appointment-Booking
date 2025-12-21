ee9d3053e18fddfc5b1c5b50eee02e55e80925ff
Siddharth Kathroju <siddharthkathuroju@gmail.com>
2025-12-22 00:23:16 +0530

    commit

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,5 +1,6 @@
+'use client';
 import { Suspense } from 'react';
-import RegisterClient from './RegisterClient.tsx';
+import RegisterClient from '/RegisterClient.tsx';
 export default function RegisterPage() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
